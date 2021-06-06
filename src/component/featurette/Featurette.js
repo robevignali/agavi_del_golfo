706f835422421304ff21de666024053efe753221
robevignali <robevignali@gmail.com>
2021-06-06 17:14:55 +0200

    edit featurette responsivity

--- src/component/featurette/Featurette.js
+++ src/component/featurette/Featurette.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import classes from "../component.module.css"
+import classes from "./featurette.module.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row"
@@ -17,7 +17,7 @@ const featurette=(props)=>(
 <Container className={classes.feature__container}>
     <Row>
         {props.leftImg?
-            <Col md="5">
+            <Col xl={5} lg={6} className="d-none d-lg-block">
                 <Card border="light" className={classes.feature__image}>
                     <Slideshow
                         interval={100000}
@@ -30,12 +30,13 @@ const featurette=(props)=>(
             :null
         }
         
-        <Col md="7">
+        <Col xl={7} lg={6} md={12}>
             <Card border="light" className={classes.feature__text}>
 
                 <Card.Img  className={classes.feature__overlay} src="https://i.postimg.cc/tTMnYczw/8rgt-n9jh-130928.jpg" /> 
                 <Card.ImgOverlay>
-                    <Card.Title style={{margin:"0", paddingTop:props.spacing}}>
+                    {/* <Card.Title style={{margin:"0", paddingTop:props.spacing}}> */}
+                    <Card.Title>
                         <h1>{props.title}</h1>
                     </Card.Title>
                     <Card.Body style={{padding:"0"}}>
@@ -49,7 +50,7 @@ const featurette=(props)=>(
         </Col>
         
         {props.rightImg?
-            <Col md="5">
+            <Col xl={5} lg={6} className="d-none d-lg-block">
                 <Card border="light" className={classes.feature__image}>
                     <Slideshow
                         interval={100000}
@@ -62,26 +63,30 @@ const featurette=(props)=>(
             :null
         }
     </Row>
-    {props.map?
-        <Map
-            height={props.mapHeight}
-            width={'100%'}
-            places={props.places}
-            center={props.map_center}
-            zoom={props.map_zoom}
-        />
-    
-    :null}
-    {props.surroundingMap?
-        <SurroundingMap
-            height={props.mapHeight}
-            width={'100%'}
-            places={props.places}
-            center={props.map_center}
-            zoom={props.map_zoom}
-        />
-    
-    :null}
+    <Row>
+        <Col xl={12}>    
+            {props.map?
+                <Map
+                    height={props.mapHeight}
+                    width={'100%'}
+                    places={props.places}
+                    center={props.map_center}
+                    zoom={props.map_zoom}
+                />
+            
+            :null}
+            {props.surroundingMap?
+                <SurroundingMap
+                    height={props.mapHeight}
+                    width={'100%'}
+                    places={props.places}
+                    center={props.map_center}
+                    zoom={props.map_zoom}
+                />
+            
+            :null}
+        </Col>
+    </Row>
 </Container>
 </div>
 )
