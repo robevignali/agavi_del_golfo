13742d4f0a4dba1000d77975138601da196186c6
robevignali <robevignali@gmail.com>
2021-06-20 12:16:41 +0200

    merged pro gallery module

--- src/container/apartment gallery/Apartment_gallery.js
+++ src/container/apartment gallery/Apartment_gallery.js
@@ -8,6 +8,9 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Modal from "../../component/Modal/Modal";
 import Head from "../../component/Head gallery/Head";
+import { ProGallery } from 'pro-gallery';
+import 'pro-gallery/dist/statics/main.css';
+// import ProGallery from "../../component/pro_gallery/pro_gallery"
 
 class Gallery extends Component {
 
@@ -29,11 +32,22 @@ class Gallery extends Component {
     }
 
     componentDidMount() {
+        // var downloadedValueArray[];
         var storageRef = firebase.storage().ref();
         storageRef.child('apartment/').listAll().then((res)=>{
             res.items.forEach((item)=>{
                 item.getDownloadURL().then((url)=>{
-                this.setState({picGalleryData : [...this.state.picGalleryData,{image:url}]})
+                this.setState({picGalleryData : [...this.state.picGalleryData,
+                    {mediaUrl:url},
+                    {metaData: {
+                        type: 'image',
+                        height: 200,
+                        width: 100,
+                        title: 'sample-title',
+                        description: 'sample-description',
+                        focalPoint: [0, 0],
+                    }}
+                ]})
               })
             })
         })
@@ -41,18 +55,35 @@ class Gallery extends Component {
 
     render(){
 
-        const images=[...this.state.picGalleryData];
-        const picts=images.map((obj,index)=>
-            <Col xs={6} md={3}>
-                <Image
-                    index={index} 
-                    onClick={()=>this.openModal(index)}
-                    src={obj.image} 
-                    thumbnail 
-                    className={classes.gallery__picture} 
-                />
-            </Col>
-        );
+        const items=[...this.state.picGalleryData];
+
+        // const picts=images.map((obj,index)=>
+        //     <Col xs={6} md={3}>
+        //         <Image
+        //             index={index} 
+        //             onClick={()=>this.openModal(index)}
+        //             src={obj.mediaUrl} 
+        //             thumbnail 
+        //             className={classes.gallery__picture} 
+        //         />
+        //     </Col>
+        // );
+
+        const options = {
+            galleryLayout: 0,
+            // gridStyle: 1,
+            hoveringBehaviour: 'NEVER_SHOW',
+            imageHoverAnimation: 'ZOOM_IN',
+        };
+
+        const container = {
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+
+        const scrollingElement = window;
+
+        const eventsListener = (eventName, eventData) => console.log({eventName, eventData});
 
         return (
             <>
@@ -70,11 +101,15 @@ class Gallery extends Component {
                         The Apartment
                     </h1>
                 </div>
-                <Container>
-                    <Row>
-                    {picts}
-                    </Row>
-                </Container>
+                {/* <Container> */}
+                    <ProGallery
+                        items={items}
+                        options={options}
+                        container={container}
+                        eventsListener={eventsListener}
+                        scrollingElement={scrollingElement}
+                    />
+                {/* </Container> */}
             </div>
             </>
         ) 
